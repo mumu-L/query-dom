904c08e75f02b545b43a3afea505e919ac1b95f6
lilin <178732414@qq.com>
2022-04-27 17:34:44 +0800

    完成基本功能

--- src/components/query/el_json.vue
+++ src/components/query/el_json.vue
@@ -21,7 +21,20 @@
     >
       <a-input v-model:value="formState.name" />
     </a-form-item>
-
+    <a-form-item
+      label="key"
+      name="key"
+      :rules="[{ required: true, message: '请输入元素名' }]"
+    >
+      <a-input v-model:value="formState.key" />
+    </a-form-item>
+    <a-form-item
+      label="value"
+      name="value"
+      :rules="[{ required: true, message: '请输入元素名' }]"
+    >
+      <a-input v-model:value="formState.value" />
+    </a-form-item>
     <a-form-item
       label="结果"
       name="textarea2"
@@ -37,6 +50,7 @@
       <a-button type="primary" html-type="submit" id="submitBtn" @click="isClick()">Submit</a-button>
     </a-form-item>
   </a-form>
+  <code  id="text"></code>
   
 </template>
 
@@ -47,7 +61,8 @@ import { reactive } from "vue"
 let formState=reactive({
     type:'id',
     name:'',
-    password:'',
+    key:'',
+    value:'',
     textarea:''
 })
 
@@ -60,20 +75,38 @@ let isClick =  async ()=>{
     function: ()=>{
 
       chrome.storage.sync.get('formState',({ formState }) => {
-
+          let obj = {}
           if(formState.type=='id'){
-            let getEl = document.getElementById(formState.name).innerHTML
-            console.log(getEl)
-            chrome.storage.sync.set({ el:getEl });
+            let getEl = document.getElementById(formState.name).getElementsByTagName('option')
+            for(let i = 0;i<getEl.length;i++){
+              obj[getEl[i].getAttribute('value')] = getEl[i].label
+            }
+            
           }
+          console.log(obj)
+          // if(formState.type=='class'){
+          //   let getEl = document.getElementByClassName(formState.name).innerHTML
+          // }
+          chrome.storage.sync.set({ el:obj });
       })
     },
   });
   chrome.storage.sync.get('el',({ el }) => {
-    console.log(el)
-    formState.textarea = el
+    // console.log(el)
+    // const obj ={}
+    // let keys = el.match(`/${formState.key}/g`)
+    // let values = el.match(`/${formState.value}/g`)
+    // keys.forEach((f,i)=>{
+    //   obj[f] = values[i]
+    // })
+    // for(let (k,i) in keys){
+      
+    // }
+    formState.textarea = JSON.stringify(el)
+    document.getElementById('text').innerHTML=JSON.stringify(el)
     // document.getElementById('text').text = el
   })
+  
 }
 
 // let btn =document.getElementById('submitBtn')
@@ -94,6 +127,8 @@ function query(){
     console.log(formState.type)
     console.log(formState.name)
     const el = document.getElementById(formState.name)
+
+
     console.log(el)
     document.getElementById('text').append(el)
   }
